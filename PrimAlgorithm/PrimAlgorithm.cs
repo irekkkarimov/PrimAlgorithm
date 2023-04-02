ed800cfdd4d496ae024f410ceca21ef352bff56b
irekkkarimov <irekkkarimov@mail.ru>
2023-04-02 23:50:39 +0300

    ready algorithm + ToMatrixConverter + so on

---
+++ PrimAlgorithm/PrimAlgorithm.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimAlgorithm;
+public class PrimAlgorithm 
+{
+    public int V;  // Число вершин графа
+    public int[][] graph;  // Матрица смежности для графа
+
+    public PrimAlgorithm(int[][] g) 
+    {
+        V = g.Length;
+        graph = g;
+    }
+
+    public int MinKey(int[] key, bool[] mstSet) 
+    {
+        int min = int.MaxValue, minIndex = -1;
+
+        for (int v = 0; v < V; v++)  
+        {
+            if (!mstSet[v] && key[v] < min) 
+            {
+                min = key[v];
+                minIndex = v;
+            }
+        }
+
+        return minIndex;
+    }
+
+    public int[] PrimMST()  
+    {
+        int[] parent = new int[V];
+        int[] key = new int[V];
+        bool[] mstSet = new bool[V];
+        int mstWeight = 0;
+
+        for (int i = 0; i < V; i++) 
+        {
+            key[i] = int.MaxValue;
+            mstSet[i] = false;
+        }
+
+        key[0] = 0; // Начинаем с первой вершины
+        parent[0] = -1;
+
+        for (int count = 0; count < V - 1; count++) 
+        {
+            int u = MinKey(key, mstSet);
+
+            mstSet[u] = true;
+
+            for (int v = 0; v < V; v++) 
+            {
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v])
+                {
+                    parent[v] = u;
+                    key[v] = graph[u][v];
+                }
+            }
+        }
+
+        return parent;
+    }
+}
---
+++ PrimAlgorithm/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace PrimAlgorithm;
+using System.Diagnostics;
+
+public static class Program
+{
+    public static void Main()
+    {
+        
+        int[][] graph =
+        {
+            new int[] { 0, 2, 0, 6, 0 },
+            new int[] { 2, 0, 3, 8, 5 },
+            new int[] { 0, 3, 0, 0, 7 },
+            new int[] { 6, 8, 0, 0, 9 },
+            new int[] { 0, 5, 7, 9, 0 }
+        };
+
+        var stopWatch = new Stopwatch();
+        var arr = Initializer();
+        
+        // for (var i = 0; i < 50; i++)
+        // {
+        //     int[][] usedGraph;
+        //     usedGraph = ConvertToMatrix(arr[i]);;
+        //
+        //     PrimAlgorithm mst = new PrimAlgorithm(usedGraph);
+        //
+        //     stopWatch.Start();
+        //     int[] parent = mst.PrimMST();
+        //     stopWatch.Stop();
+        // }
+        
+        var singleUsedGraph = graph;;
+
+        PrimAlgorithm mstSingle = new PrimAlgorithm(singleUsedGraph);
+
+        stopWatch.Start();
+        int[] parentSingle = mstSingle.PrimMST();
+        stopWatch.Stop();
+
+        Console.WriteLine("Edge   Weight");
+        for (int i = 1; i < mstSingle.V; i++)
+        {
+            Console.WriteLine(parentSingle[i] + " - " + i + "    " + singleUsedGraph[i][parentSingle[i]]);
+        }
+        Console.WriteLine("------");
+        Console.WriteLine($"Time elapsed : {stopWatch.Elapsed.Milliseconds}");
+    }
+
+    public static string[] Initializer()
+    {
+        var file = File.ReadAllText(@"C:\Users\Booba\Desktop\100_graphs_with_vertices_from_100_to_10000.txt");
+        var arr = file.Split("\n\n\n");
+        return arr;
+    }
+
+    public static int[][] ConvertToMatrix(string iGraph)
+    {
+        var firstEdges = iGraph.Split('\n');
+        int[][] splittedEdges = new int[firstEdges.Length][];
+        var max = int.MinValue;
+        
+        for (var i = 0; i < firstEdges.Length; i++)
+        {
+            var temp = firstEdges[i]
+                .Replace("[", string.Empty)
+                .Replace("]", string.Empty)
+                .Split(", ");
+
+            splittedEdges[i] = new int[] { int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]) };
+            if (splittedEdges[i][0] > max || splittedEdges[i][1] > max)
+                max = Math.Max(splittedEdges[i][0], splittedEdges[i][1]);
+        }
+        var verticesCount = 0;
+        max++;
+        
+        int[][] matrix = new int[max][];
+    
+        for (var e = 0; e < max; e++)
+            matrix[e] = new int[max];
+
+        foreach (var line in splittedEdges)
+        {
+            matrix[line[0]][line[1]] = line[2];
+            matrix[line[1]][line[0]] = line[2];
+        }
+
+        return matrix;
+    }
+}
